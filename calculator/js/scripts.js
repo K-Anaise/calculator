bba9f3d27ca99ca22c3f3681214a3b369b668b73
Grace <tesigrace98@gmail.com>
2019-07-17 11:59:54 +0200

    added js codes

---
+++ calculator/js/scripts.js
@@ -0,0 +1,10 @@
+var subtract = function(number1, number2) {
+  return number1 - number2;
+  };
+  alert(subtract(10,5));
+  ar add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = prompt("Enter a number:");
+  var number2 = prompt("Enter another number:");
+  alert(add(number1, number2));  
